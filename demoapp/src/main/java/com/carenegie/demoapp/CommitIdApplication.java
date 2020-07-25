23c1dcc67ffb90f2a53cbc83e091a59aede2a4c6
giriadini <giri62535@gmail.com>
2020-07-25 11:58:10 +1000

    Intial Commit

---
+++ demoapp/src/main/java/com/carenegie/demoapp/CommitIdApplication.java
@@ -0,0 +1,25 @@
+package com.carenegie.demoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.io.ClassPathResource;
+
+@SpringBootApplication(scanBasePackages = { "com.carenegie.demoapp" })
+public class CommitIdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommitIdApplication.class, args);
+    }
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer placeholderConfigurer() {
+        PropertySourcesPlaceholderConfigurer propsConfig
+                = new PropertySourcesPlaceholderConfigurer();
+        propsConfig.setLocation(new ClassPathResource("git.properties"));
+        propsConfig.setIgnoreResourceNotFound(true);
+        propsConfig.setIgnoreUnresolvablePlaceholders(true);
+        return propsConfig;
+    }
+}
